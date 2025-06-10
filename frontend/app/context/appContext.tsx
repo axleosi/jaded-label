ef50fa74507db4c41ec42f5c038caf46e3044cd3
Mbonu Osi <axleositech@gmail.com>
2025-06-10 18:57:25 +0100

    Run builds

--- frontend/app/context/appContext.tsx
+++ frontend/app/context/appContext.tsx
@@ -9,7 +9,7 @@ import React, {
 } from 'react';
 import axios from 'axios';
   import {jwtDecode} from 'jwt-decode';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 type Product = {
   _id: string;
